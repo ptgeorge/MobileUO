3a1426f6522f76b68875f315624c89f7baa4cdee
Phillip T. George <github@me.phillipgeorge.com>
2024-11-23 16:46:32 -0600

    Got rid of the massive amounts of warnings by getting rid of XNAEmulator

--- Assets/Scripts/XNAEmulator/Audio/AudioChannels.cs
+++
@@ -1,8 +0,0 @@
-namespace Microsoft.Xna.Framework.Audio
-{
-    public enum AudioChannels
-    {
-        Mono = 1,
-        Stereo = 2,
-    }
-}
--- Assets/Scripts/XNAEmulator/Audio/SoundEffect.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Microsoft.Xna.Framework.Audio
-{
-    public static class SoundEffect
-    {
-        public static float MasterVolume { get; set; }
-    }
-}
